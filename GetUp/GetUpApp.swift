f86e55e6d22693ca2ad1a7ae9ff309fe58cc9991
Lindsay Schwisow <lindsay.schwisow@chsd117.org>
2021-04-12 18:22:11 -0500

    Initial Commit

---
+++ GetUp/GetUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetUpApp.swift
+//  GetUp
+//
+//  Created by Lindsay Schwisow on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GetUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
